6679bebd9215d51dd9a6f9b4bbf287b67780993c
Andrew Lohr <lohrandrew@icloud.com>
2025-02-01 09:38:05 -0500

    Add drivestates and autochooser from kitbot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,19 +8,22 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-import frc.robot.constants.SubsystemConstants;
 import frc.robot.constants.TunerConstants;
-import frc.robot.subsystems.algaeManipulator.states.AlgaeIntake;
 import frc.robot.subsystems.drivetrain.CommandSwerveDrivetrain;
 import frc.robot.subsystems.drivetrain.states.DriveState;
+import frc.robot.subsystems.drivetrain.states.ResetHeadingState;
+import static frc.robot.constants.FieldConstants.SIDE_CHOOSER;
+import static frc.robot.constants.FieldConstants.getGlobalPositions;
+import static frc.robot.constants.SubsystemConstants.DrivetrainConstants.TeleConstants.MAX_TRANSLATION;
 
 public class RobotContainer {
     private ShuffleboardTab autoTab;
     private SendableChooser<Command> autoChooser;
 
-    private final Telemetry logger = new Telemetry(SubsystemConstants.DrivetrainConstants.TeleConstants.MAX_TRANSLATION); //TODO: maybe remove this?
+    private final Telemetry logger = new Telemetry(MAX_TRANSLATION);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
 
@@ -31,13 +34,17 @@ public class RobotContainer {
         registerNamedCommands();
         setupAutos();
     }
-
+        
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            new DriveState(drivetrain, joystick::getLeftY, joystick::getLeftX, joystick::getRightX)
+            new DriveState(
+                drivetrain, 
+                joystick::getLeftY,
+                joystick::getLeftX,
+                joystick::getRightX
+            )
         );
 
         // Run SysId routines when holding back/start and X/Y.
@@ -48,26 +55,47 @@ public class RobotContainer {
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        joystick.b().onTrue(new ResetHeadingState(drivetrain));
 
+        // joystick.a().whileTrue(new PathfindingState(drivetrain, getGlobalPositions().CORAL_STATION_LEFT));
+        // joystick.y().whileTrue(new PathfindingState(drivetrain, getGlobalPositions().CORAL_EF));
 
-        //joystick.rightBumper().whileTrue(new AlgaeIntake(algaeManipulator));
+        if(!Robot.isReal()) {
+            drivetrain.registerTelemetry(logger::telemeterize);
+        }
 
+        configureButtons();
+    }
 
-        drivetrain.registerTelemetry(logger::telemeterize);
+    private void configureButtons() {
+        var buttonTab = Shuffleboard.getTab("Buttons");
+        
+        buttonTab.add("AB", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_AB)).ignoringDisable(true));
+        buttonTab.add("CD", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_CD)).ignoringDisable(true));
+        buttonTab.add("EF", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_EF)).ignoringDisable(true));
+        buttonTab.add("GH", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_GH)).ignoringDisable(true));
+        buttonTab.add("IJ", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_IJ)).ignoringDisable(true));
+        buttonTab.add("KL", new InstantCommand(() -> drivetrain.setNextScorePose(getGlobalPositions().CORAL_KL)).ignoringDisable(true));
+
+        buttonTab.add("Left", new InstantCommand(() -> drivetrain.setNextFeedPose(getGlobalPositions().CORAL_STATION_LEFT)).ignoringDisable(true));
+        buttonTab.add("Right", new InstantCommand(() -> drivetrain.setNextFeedPose(getGlobalPositions().CORAL_STATION_RIGHT)).ignoringDisable(true));
     }
 
     private void registerNamedCommands() {
-        // NamedCommands.registerCommand("Drop", new DropState(dropper)); example
+        // NamedCommands.registerCommand("Drop", new DropState(dropper).withTimeout(0.5)); //ex
     }
 
     private void setupAutos() {
         autoChooser = new SendableChooser<>();
+
+        SIDE_CHOOSER.setDefaultOption("Red", "red");
+        SIDE_CHOOSER.addOption("Blue", "blue");
     
         autoTab = Shuffleboard.getTab("Auto");
-        autoTab.add(autoChooser).withSize(2, 1);
+        autoTab.add("AutoChooser", autoChooser);
+        autoTab.add("SideChooser", SIDE_CHOOSER);
 
-        // autoChooser.setDefaultOption("5 coral!!!", new PathPlannerAuto("5 coral!!!")); example
+        // autoChooser.setDefaultOption("5 coral!!!", new PathPlannerAuto("5 coral!!!")); //ex
     }
 
     public Command getAutonomousCommand() {
