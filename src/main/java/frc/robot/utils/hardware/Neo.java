acffa855cb1c80f8ab9c620ca35c3c8ccf2f5168
Andrew Lohr <lohrandrew@icloud.com>
2025-02-01 12:27:22 -0500

    add setreference with a feedforward param

--- src/main/java/frc/robot/utils/hardware/Neo.java
+++ src/main/java/frc/robot/utils/hardware/Neo.java
@@ -3,6 +3,8 @@ package frc.robot.utils.hardware;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 import com.revrobotics.spark.SparkMax;
 
 import frc.robot.utils.logging.Loggable;
@@ -20,6 +22,10 @@ public class Neo extends SparkMax implements Loggable {
 
     public Neo(int id) {
         super(id, MotorType.kBrushless);
+    }
+
+    public void setReference(double reference, ControlType controlType, ClosedLoopSlot slot, double ff) {
+        getClosedLoopController().setReference(reference, controlType, slot, ff, ArbFFUnits.kPercentOut);
     }
 
     public void setReference(double reference, ControlType controlType) {
--- src/main/java/frc/robot/utils/hardware/Vortex.java
+++ src/main/java/frc/robot/utils/hardware/Vortex.java
@@ -3,7 +3,9 @@ package frc.robot.utils.hardware;
 import org.littletonrobotics.junction.AutoLog;
 import org.littletonrobotics.junction.Logger;
 
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkClosedLoopController.ArbFFUnits;
 
 import frc.robot.utils.logging.Loggable;
 
@@ -20,6 +22,10 @@ public class Vortex extends SparkFlex implements Loggable {
 
     public Vortex(int id) {
         super(id, MotorType.kBrushless);
+    }
+
+    public void setReference(double reference, ControlType controlType, ClosedLoopSlot slot, double ff) {
+        getClosedLoopController().setReference(reference, controlType, slot, ff, ArbFFUnits.kPercentOut);
     }
 
     public void setReference(double reference, ControlType controlType) {
