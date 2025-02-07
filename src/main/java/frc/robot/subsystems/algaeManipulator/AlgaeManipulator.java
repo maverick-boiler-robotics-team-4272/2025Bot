096fb6b93266b33a3fad25bf592ea8a693eca7ac
Andrew Lohr <48129314+alohr86@users.noreply.github.com>
2025-02-06 22:39:26 -0500

    Add absolute encoder config

--- src/main/java/frc/robot/subsystems/algaeManipulator/AlgaeManipulator.java
+++ src/main/java/frc/robot/subsystems/algaeManipulator/AlgaeManipulator.java
@@ -5,19 +5,27 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.constants.HardwareMap.*;
 
 import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.spark.config.AbsoluteEncoderConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import frc.robot.utils.hardware.Vortex;
 import frc.robot.utils.hardware.VortexBuilder;
 
+import static frc.robot.constants.SubsystemConstants.ArmevatorConstants.*;
+
 public class AlgaeManipulator extends SubsystemBase {
     private Vortex algaeControllerMotor;
 
     public AlgaeManipulator() {
+        AbsoluteEncoderConfig encoderConfig = new AbsoluteEncoderConfig()
+            .positionConversionFactor(MAV_2_POSITION_FACTOR);
+
+
         algaeControllerMotor = VortexBuilder.create(ALGAE_MOTOR_ID)
             .withInversion(false)
-            .withCurrentLimit(80)
+            .withCurrentLimit(40)
             .withIdleMode(IdleMode.kBrake)
+            .withAbsoluteEncoderConfig(encoderConfig)
             .build();
     }
 
