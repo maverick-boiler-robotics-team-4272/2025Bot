c0c1f2558c097f4d2428b53a809ffcf7cbcf502d
Andrew Lohr <lohrandrew@icloud.com>
2025-02-15 07:39:47 -0500

    Change the folder name for the armevator

--- src/main/java/frc/robot/subsystems/armevator/States/GoToArmevatorPoseState.java
+++ src/main/java/frc/robot/subsystems/armevator/States/GoToArmevatorPoseState.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.armevator.States;
+package frc.robot.subsystems.armevator.states;
 
 import frc.robot.constants.positions.ArmevatorPosition;
 import frc.robot.subsystems.armevator.Armevator;
