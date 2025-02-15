7dc11f976b1c6a9af066a5b6a6cf8ac2c3e1b582
Kramarth <edlazars@gmail.com>
2025-02-15 10:55:03 -0500

    add semi-functional feed and manipulator command

--- src/main/java/frc/robot/commands/FeederManipulatorCommand.java
+++ src/main/java/frc/robot/commands/FeederManipulatorCommand.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 // Commands / States
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -12,12 +13,16 @@ import frc.robot.subsystems.coralManipulator.*;
 
 
 public class FeederManipulatorCommand extends SequentialCommandGroup {
-    public FeederManipulatorCommand(Feeder feed, CoralManipulator coralManipulator, double feedPower, double coralPower) {
+    public FeederManipulatorCommand(Feeder feed, CoralManipulator coralManipulator, double feedPower, double coralPower, double rotation) {
         super(
             new ParallelRaceGroup(
                 new FeedState(feed, feedPower),
                 new CoralIntakeState(coralManipulator, coralPower)
-            ).until(feed::lidarFrontTripped)
+            ).until(feed::lidarBackTripped),
+            new ParallelRaceGroup(
+                new FeedState(feed, feedPower),
+                new CoralIntakeState(coralManipulator, coralPower)
+            ).until(feed::lidarBackNotTripped)            
         );
     }
 }
