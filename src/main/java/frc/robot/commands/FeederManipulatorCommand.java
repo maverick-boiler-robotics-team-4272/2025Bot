4605ec34517cfff5e6ca4b3246f2f45cc5d5aa6d
Kramarth <edlazars@gmail.com>
2025-02-15 13:40:15 -0500

    add armevator position while feeding guh

--- src/main/java/frc/robot/commands/FeederManipulatorCommand.java
+++ src/main/java/frc/robot/commands/FeederManipulatorCommand.java
@@ -5,23 +5,29 @@ import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.coralManipulator.states.*;
 import frc.robot.subsystems.feeder.states.*;
+import frc.robot.subsystems.armevator.states.*;
 
 // Subsystems
 import frc.robot.subsystems.feeder.*;
 import frc.robot.subsystems.coralManipulator.*;
+import frc.robot.constants.positions.ArmevatorPositions.ArmevatorPosition;
+import frc.robot.subsystems.armevator.*;
 
 
 public class FeederManipulatorCommand extends SequentialCommandGroup {
-    public FeederManipulatorCommand(Feeder feed, CoralManipulator coralManipulator, double feedPower, double coralPower, double rotation) {
+    public FeederManipulatorCommand(Feeder feed, CoralManipulator coralManipulator, Armevator armevator, double feedPower, double coralPower, ArmevatorPosition position) {
         super(
             new ParallelRaceGroup(
-                new FeedState(feed, feedPower),
-                new CoralIntakeState(coralManipulator, coralPower)
-            ).until(feed::lidarBackTripped),
-            new ParallelRaceGroup(
-                new FeedState(feed, feedPower),
-                new CoralIntakeState(coralManipulator, coralPower)
-            ).until(feed::lidarBackNotTripped)            
+                new GoToArmevatorPoseState(armevator, position),
+                new ParallelRaceGroup(
+                    new FeedState(feed, feedPower),
+                    new CoralIntakeState(coralManipulator, coralPower)
+                ).until(feed::lidarBackTripped),
+                new ParallelRaceGroup(
+                    new FeedState(feed, feedPower),
+                    new CoralIntakeState(coralManipulator, coralPower)
+                ).until(feed::lidarBackNotTripped)
+            ).until(feed::lidarBackNotTripped)
         );
     }
 }
