ee54a1a1f5b46f32246c1bf3fb9262443657f969
Andrew Lohr <lohrandrew@icloud.com>
2025-02-20 20:19:24 -0500

    Adjust constant

--- src/main/java/frc/robot/subsystems/coralManipulator/states/IdleState.java
+++ src/main/java/frc/robot/subsystems/coralManipulator/states/IdleState.java
@@ -23,6 +23,6 @@ public class IdleState extends State<CoralManipulator> {
 
     @Override
     public void execute() {
-        requiredSubsystem.setWheelRotation(currentArmRotation.get().minus(startArmRotation).times(4));
+        requiredSubsystem.setWheelRotation(currentArmRotation.get().minus(startArmRotation).times(7.0));
     }
 }
