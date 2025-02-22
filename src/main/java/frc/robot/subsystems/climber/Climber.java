82b00ee93ec4f6bcbbe4c451a08b4e97d56f80b4
Andrew Lohr <lohrandrew@icloud.com>
2025-02-22 13:32:25 -0500

    Add logging

--- src/main/java/frc/robot/subsystems/climber/Climber.java
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -7,10 +7,11 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.hardware.Vortex;
 import frc.robot.utils.hardware.VortexBuilder;
+import frc.robot.utils.logging.Loggable;
 
 import static frc.robot.constants.SubsystemConstants.ClimberConstants.*;
 
-public class Climber extends SubsystemBase {
+public class Climber extends SubsystemBase implements Loggable {
     private Vortex climberControllerMotor;
 
     public Climber() {
@@ -22,5 +23,15 @@ public class Climber extends SubsystemBase {
 
     public void setClimberPower(double power) {
         climberControllerMotor.set(power);
+    }
+
+    @Override
+    public void log(String subdirectory, String humanReadableName) {
+        climberControllerMotor.log(subdirectory + "/" + humanReadableName, "CLimberMotor");
+    }
+
+    @Override
+    public void periodic() {
+        log("Subsystems", "Climber");
     }
 }
