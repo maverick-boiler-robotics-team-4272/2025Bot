c835a5cb71a2aedd7896dd9e2f600049595e385c
Andrew Lohr <lohrandrew@icloud.com>
2025-03-01 12:31:35 -0500

    Make it hold the algae if it goes down
    WARNING NEEDS ADJUSTED

--- src/main/java/frc/robot/subsystems/algaeManipulator/states/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/algaeManipulator/states/AlgaeIntake.java
@@ -17,7 +17,7 @@ public class AlgaeIntake extends State<AlgaeManipulator> {
 
   public void execute() {
     if (requiredSubsystem.hasAlgae()) {
-      requiredSubsystem.setAlgaePower(0.2);
+      requiredSubsystem.setAlgaePower(0.5);
     } else {
       requiredSubsystem.setAlgaePower(1.0);
     }
