6c38f0c12a6d24cbf58e5ff2c946b3a5d038fce4
Andrew Lohr <lohrandrew@icloud.com>
2025-03-04 18:05:32 -0500

    Set current limit higher

--- src/main/java/frc/robot/constants/TunerConstants.java
+++ src/main/java/frc/robot/constants/TunerConstants.java
@@ -61,7 +61,7 @@ public class TunerConstants {
             new CurrentLimitsConfigs()
                 // Swerve azimuth does not require much torque output, so we can set a relatively low
                 // stator current limit to help avoid brownouts without impacting performance.
-                .withStatorCurrentLimit(Amps.of(30))//40
+                .withStatorCurrentLimit(Amps.of(40))//40
                 .withStatorCurrentLimitEnable(true)
         );
     private static final CANcoderConfiguration encoderInitialConfigs = new CANcoderConfiguration();
