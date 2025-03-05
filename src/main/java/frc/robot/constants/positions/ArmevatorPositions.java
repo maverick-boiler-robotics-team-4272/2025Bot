5ad0c5fd09c6d9a24069edd5ada2c238339c94ad
Andrew Lohr <lohrandrew@icloud.com>
2025-03-04 20:15:01 -0500

    Adjust armevator poses

--- src/main/java/frc/robot/constants/positions/ArmevatorPositions.java
+++ src/main/java/frc/robot/constants/positions/ArmevatorPositions.java
@@ -20,8 +20,8 @@ public class ArmevatorPositions {
     public static final ArmevatorPosition BARGE_PREP_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(100), Meters.convertFrom(50,Inches));
 
     public static final ArmevatorPosition L1_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(47), Meters.convertFrom(0, Inches));
-    public static final ArmevatorPosition L2_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(30), Meters.convertFrom(11.5, Inches));
-    public static final ArmevatorPosition L3_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(20), Meters.convertFrom(30.5, Inches));
+    public static final ArmevatorPosition L2_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(15), Meters.convertFrom(16.0, Inches));
+    public static final ArmevatorPosition L3_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(15), Meters.convertFrom(32.5, Inches));
     public static final ArmevatorPosition L4_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(147.775 - 360.0), Meters.convertFrom(30, Inches));
     // public static final ArmevatorPosition L4_ARMEVATOR_POSITION = new ArmevatorPosition(Rotation2d.fromDegrees(147.775), Meters.convertFrom(30, Inches));
 
