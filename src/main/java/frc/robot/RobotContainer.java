e1cde05ad1344161749001ac6932d957a724c5d2
Andrew Lohr <lohrandrew@icloud.com>
2025-03-14 10:48:03 -0400

    push auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -487,6 +487,7 @@ public class RobotContainer {
         autoChooser.addOption("Wheel Diam", new PathPlannerAuto("Wheel Diam"));
         autoChooser.addOption("Left Auto", new PathPlannerAuto("Left Two Piece auto", false));
         autoChooser.addOption("Right Auto", new PathPlannerAuto("Right Two Piece auto", false));
+        autoChooser.addOption("Left Robot Push", new PathPlannerAuto("Push Left Robot", false));
         autoChooser.setDefaultOption("Middle Auto", new PathPlannerAuto("Short Auto", false));
         // autoChooser.setDefaultOption("Output name", new PathPlannerAuto("auto name", boolean mirror same field)); //ex
     }
