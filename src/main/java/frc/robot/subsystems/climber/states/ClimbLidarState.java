9d012700a5b0377ab3582c1d43560a3b3477b796
Andrew Lohr <lohrandrew@icloud.com>
2025-03-16 17:00:16 -0400

    Correct the climb lidar state

--- src/main/java/frc/robot/subsystems/climber/states/ClimbLidarState.java
+++ src/main/java/frc/robot/subsystems/climber/states/ClimbLidarState.java
@@ -1,21 +1,10 @@
 package frc.robot.subsystems.climber.states;
 
-public class ClimbLidarState {
-
 import frc.robot.subsystems.climber.Climber;
 import frc.robot.utils.commandUtils.State;
 
-  public FeedLidarState(Feeder feederSubsystem) {
-    super(feederSubsystem);
-  }
-
-  @Override
-  public void initialize() {
-    requiredSubsystem.setFeederPower(1);
-  }
-
-  @Override
-  public void end(boolean interrupted) {
-    requiredSubsystem.setFeederPower(1);
+public class ClimbLidarState extends State<Climber> {
+  public ClimbLidarState(Climber climber) {
+    super(climber);
   }
 }
